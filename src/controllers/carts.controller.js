9ac2790e37617fb38cd6a48276667b3e4b68cbe8
ledesmamanu <ledesmamanu345@gmail.com>
2023-07-11 13:59:06 -0300

    Aplicando arquitectura

---
+++ src/controllers/carts.controller.js
@@ -0,0 +1,45 @@
+import CartManager from "../../dao/mongo/Managers/cartsManager.js";
+import { REQUEST_STATUS } from "../consts.js";
+
+const cartService = new CartManager();
+
+const createCart = async (req,res)=>{
+    const cart = {
+        products:[]
+    }
+    const result = await cartService.createCart(cart)
+    res.send({status:"Success", message:"Cart was created successfuly"})
+}
+
+const addProductToCart = async (req,res)=>{
+    const {cid} = req.params;
+    const {pid} = req.params;
+    const cart = await cartService.getCartBy({_id:cid})
+    const products = {
+        product:pid
+    }
+
+    const result = await cartService.setProductToCart(cid, products)
+    res.send({status:"Success", message:"Product added successfuly"})
+
+}
+
+const getCarts = async (req,res)=>{
+    const carts = await cartService.getCarts();
+    console.log(JSON.stringify(carts[0]._id))
+    res.send(carts)
+}
+
+const getCartBy =  async (req,res)=>{
+    const {cid} = req.params;
+    const result = await cartService.getCartBy({_id:cid})
+    console.log(result)
+    res.send({status:"Success", payload:result})
+}
+
+export default {
+    createCart,
+    addProductToCart,
+    getCarts,
+    getCartBy
+}
--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -1,47 +1,16 @@
 import { Router } from "express";
-import CartManager from "../../dao/mongo/Managers/cartsManager.js";
-import ProductsManager from "../../dao/mongo/Managers/productsManager.js";
-import { REQUEST_STATUS } from "../consts.js";
+import cartsController from "../controllers/carts.controller.js";
 
-const cartService = new CartManager();
-const productService = new ProductsManager();
 
 const router = Router();
 
 //METODO POST
-
-router.post('/', async (req,res)=>{
-    const cart = {
-        products:[]
-    }
-    const result = await cartService.createCart(cart)
-    res.send({status:"Success", message:"Cart was created successfuly"})
-})
-router.post('/:cid/product/:pid', async (req,res)=>{
-    const {cid} = req.params;
-    const {pid} = req.params;
-    const cart = await cartService.getCartBy({_id:cid})
-    const products = {
-        product:pid
-    }
-
-    const result = await cartService.setProductToCart(cid, products)
-    res.send({status:"Success", message:"Product added successfuly"})
-
-})
+router.post('/',cartsController.createCart)
+router.post('/:cid/product/:pid',cartsController.addProductToCart)
 
 
 //METODO GET
-router.get('/', async (req,res)=>{
-    const carts = await cartService.getCarts();
-    console.log(JSON.stringify(carts[0]._id))
-    res.send(carts)
-})
-router.get('/:cid', async (req,res)=>{
-    const {cid} = req.params;
-    const result = await cartService.getCartBy({_id:cid})
-    console.log(result)
-    res.send({status:"Success", payload:result})
-})
+router.get('/',cartsController.getCarts)
+router.get('/:cid',cartsController.getCartBy)
 
 export default router
