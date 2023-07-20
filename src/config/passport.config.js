77707febbe333b23a1e8bd0bd5ba9ec3ff6bb2a0
ledesmamanu <ledesmamanu345@gmail.com>
2023-07-20 02:48:01 -0300

    Agregando passport

---
+++ src/config/passport.config.js
@@ -0,0 +1,2 @@
+import passport from "passport";
+import local from "passport-local";
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,17 @@
 import {fileURLToPath} from 'url';
 import { dirname } from 'path';
+import bcrypt from 'bcrypt';
+
+//Creamos el hash para la password
+//hashSync toma la contraseña que le pasemos y aplica el proceso de hasheo a partir de un "Salt"
+//genSaltSync crea una salt de 10 caracteres.
+//Este proceso devuelve un string ramdom que genera el hashSync, es IRREVERSIBLE
+export const createHash = (password)=> bcrypt.hashSync(password,bcrypt.genSaltSync(10));
+
+//isValidPassword valida si el password del user es el mismo que el hash del servidor
+export const isValidPassword = (user,password) => bcrypt.compareSync(password, user.password)
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
 export default __dirname;
